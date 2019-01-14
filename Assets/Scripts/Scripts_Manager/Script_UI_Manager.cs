31e4acd2a493302321e17a21e211624182297047
Tentapoulpe <arthur.manoux@wanadoo.fr>
2019-01-14 10:32:29 +0100

    modif ui man region

--- Assets/Scripts/Scripts_Manager/Script_UI_Manager.cs
+++ Assets/Scripts/Scripts_Manager/Script_UI_Manager.cs
@@ -7,8 +7,13 @@ public class Script_UI_Manager : MonoBehaviour
 {
     public static Script_UI_Manager Instance { get; private set; }
 
+    #region Notes
+
+    [Header ("Note")]
     public Text t_note_title;
     public Text t_note_description;
+
+    #endregion
 
     private void Awake()
     {
@@ -23,9 +28,9 @@ public class Script_UI_Manager : MonoBehaviour
         }
     }
 
-    public void ShowNote (Note_Data c_note2)
+    public void ShowNote (Note_Data c_note_to_show)
     {
-        t_note_title.text = c_note2.s_note_title;
-        t_note_description.text = c_note2.s_note_description;
+        t_note_title.text = c_note_to_show.s_note_title;
+        t_note_description.text = c_note_to_show.s_note_description;
     }
 }
