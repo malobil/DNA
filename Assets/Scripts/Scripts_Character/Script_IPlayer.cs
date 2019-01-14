6ef875cfc31f2f4cd825953de52e2179da2c8664
Kevin Ibanez <kevin.ibanezpro@gmail.com>
2019-01-14 09:33:11 +0100

    I_Player

--- Assets/Scripts/Scripts_Character/Script_IPlayer.cs
+++ Assets/Scripts/Scripts_Character/Script_IPlayer.cs
@@ -2,17 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Script_IPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float f_move_speed_horizontal = 10f ;
+    public float f_move_speed_vertical = 10f ;
+
+    #region Private variables
+
+    private Rigidbody2D player_rb;
+
+    #endregion
+
+    private void Start()
     {
-        
+        player_rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public virtual void FixedUpdate()
     {
-        
+        Move();
+    }
+
+    public virtual void Move()
+    {
+        float f_horizontal_move = Input.GetAxis("Horizontal") * f_move_speed_horizontal;
+        float f_vertical_move = Input.GetAxis("Vertical") * f_move_speed_vertical;
+
+        player_rb.velocity = new Vector2(f_horizontal_move, f_vertical_move);
+
     }
 }
