2fc1c11eefe576f85725c1895fd7c10bcba15c0d
Kevin Ibanez <kevin.ibanezpro@gmail.com>
2019-01-14 11:38:25 +0100

    Character move animation

--- Assets/Scripts/Scripts_Character/Script_IPlayer.cs
+++ Assets/Scripts/Scripts_Character/Script_IPlayer.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Script_IPlayer : MonoBehaviour
 {
+    #region Movement variables
+
     public float f_move_speed_horizontal = 10f ;
     public float f_move_speed_vertical = 10f ;
 
-    #region Private variables
-
     private Rigidbody2D player_rb;
+
+    #endregion
+
+    #region Animation variable
+
+    private Animator player_animator;
 
     #endregion
 
     private void Start()
     {
         player_rb = GetComponent<Rigidbody2D>();
+        player_animator = GetComponent<Animator>();
     }
 
     public virtual void FixedUpdate()
@@ -31,5 +38,16 @@ public class Script_IPlayer : MonoBehaviour
 
         player_rb.velocity = new Vector2(f_horizontal_move, f_vertical_move);
 
+        if(f_horizontal_move == 0 && f_vertical_move ==0)
+        {
+            player_animator.SetBool("Idle", true);
+        }
+        else
+        {
+            player_animator.SetBool("Idle", false);
+        }
+
+        player_animator.SetFloat("horizontal_movement", f_horizontal_move);
+        player_animator.SetFloat("vertical_movement", f_vertical_move);
     }
 }
