2b315423f1625d263da929c8e63a2d9843a49403
Tentapoulpe <arthur.manoux@wanadoo.fr>
2019-01-16 17:43:18 +0100

    test trigger timeline

---
+++ Assets/Scripts/Scripts_Trigger/Script_Trigger_Cinematic/Script_Cinematic_Controller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class Script_Cinematic_Controller : MonoBehaviour
+{
+    public static Script_Cinematic_Controller Instance { get; private set; }
+    private PlayableDirector p_playabledirector;
+
+    void Start()
+    {
+        p_playabledirector = GetComponent<PlayableDirector>();
+    }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayCinematic(TimelineAsset t_timeline_to_play)
+    {
+        Time.timeScale = 0f;
+        p_playabledirector.Play(t_timeline_to_play);
+    }
+}
---
+++ Assets/Scripts/Scripts_Trigger/Script_Trigger_Cinematic/Script_Cinematic_Trigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+public class Script_Cinematic_Trigger : MonoBehaviour
+{
+    public TimelineAsset t_timeline;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Script_Cinematic_Controller.Instance.PlayCinematic(t_timeline);
+        }
+    }
+}
