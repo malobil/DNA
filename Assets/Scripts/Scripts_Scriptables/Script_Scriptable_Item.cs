6c5ecb958a3ad8d0ae3b61d7484c13810e9e841a
Tentapoulpe <arthur.manoux@wanadoo.fr>
2019-01-16 11:17:08 +0100

    scriptable + WIP trigger cinematic

---
+++ Assets/Scripts/Scripts_Scriptables/Script_Scriptable_Item.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "Item", menuName = "Scriptable/Item", order = 1)]
+
+public class Script_Scriptable_Item : ScriptableObject
+{
+    public Sprite s_item_sprite;
+    public string s_item_CSV_key;
+    public int i_item_level;
+    public GameObject g_item_prefab;
+}
---
+++ Assets/Scripts/Scripts_Trigger/Script_Trigger_Cinematic/Script_Trigger_Cinematic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class Script_Trigger_Cinematic : MonoBehaviour
+{
+    public PlayableDirector p_playable_director;
+    public List <TimelineAsset> p_timeline;
+    public int i_cinematic_to_play;
+    
+    public void PlayTimeline()
+    {
+        //p_playable_director.Play (i_cinematic_to_play);
+    }
+
+
+}
