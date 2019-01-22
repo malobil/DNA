e292f5b867bc6eb7cbfd41d7eae4da63011c2520
Tentapoulpe <arthur.manoux@wanadoo.fr>
2019-01-22 14:45:02 +0100

    finish guard view

---
+++ Assets/Scripts/Scripts_Character/Script_Character_Guard/Script_Guard_View.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_Guard_View : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Script_Game_Manager.Instance.GameOver();
+        }
+    }
+}
