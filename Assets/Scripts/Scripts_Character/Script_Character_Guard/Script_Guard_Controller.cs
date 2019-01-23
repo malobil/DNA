292e59fa6381b59449e25d0951493b62eaa158a2
Kevin Ibanez <kevin.ibanezpro@gmail.com>
2019-01-23 16:13:14 +0100

    Soldiers & main menu

--- Assets/Scripts/Scripts_Character/Script_Character_Guard/Script_Guard_Controller.cs
+++ Assets/Scripts/Scripts_Character/Script_Character_Guard/Script_Guard_Controller.cs
@@ -4,12 +4,50 @@ using UnityEngine;
 
 public class Script_Guard_Controller : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D other)
+    public enum GuardBaseDirection { Up,Left,Right,Down}
+
+    public GuardBaseDirection guard_idle_direction;
+    public Transform obj_guard_sign_of_view;
+
+    private Animator anim_guard_animator;
+
+    /*private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Objet") && Script_Player.Instance.ItemIsThrowing())
         {
             MoveToTarget(other.transform);
         }
+    }*/
+
+    private void Start()
+    {
+        anim_guard_animator = GetComponent<Animator>();
+        SetGuardDirection(guard_idle_direction.ToString(""));
+    }
+
+    private void SetGuardDirection(string direction)
+    {
+        anim_guard_animator.SetTrigger(direction);
+
+        switch (guard_idle_direction.ToString(""))
+        {
+            case "Left":
+                obj_guard_sign_of_view.localRotation = Quaternion.Euler(0, 0, -90);
+                break;
+
+            case "Right":
+                obj_guard_sign_of_view.localRotation = Quaternion.Euler(0, 0, 90);
+                break;
+
+            case "Up":
+                obj_guard_sign_of_view.localRotation = Quaternion.Euler(0, 0, 180);
+                break;
+
+            case "Down":
+                obj_guard_sign_of_view.localRotation = Quaternion.Euler(0, 0, 0);
+                break;
+        }
+        
     }
 
     public void MoveToTarget(Transform t_target_position)
---
+++ Assets/Scripts/Scripts_Manager/Script_MainMenu_Manager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Script_MainMenu_Manager : MonoBehaviour
+{
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadAScene(string s_scene_to_load)
+    {
+        SceneManager.LoadScene(s_scene_to_load);
+    }
+}
