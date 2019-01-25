060e3fd27dfc8d4bab241d1c87686e7d1ada6585
Tentapoulpe <arthur.manoux@wanadoo.fr>
2019-01-25 14:36:10 +0100

    link url survey

---
+++ Assets/Scripts/Scripts_Trigger/Script_Trigger_Survey.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_Trigger_Survey : MonoBehaviour
+{
+    public string s_link_URL;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Application.OpenURL(s_link_URL);
+            Script_Game_Manager.Instance.GameOver();
+        }
+    }
+}
