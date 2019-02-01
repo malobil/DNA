bb3daa3301beac69c907594335d80d214eff81d7
Kevin Ibanez <kevin.ibanezpro@gmail.com>
2019-02-01 09:32:37 +0100

    Opti

--- Assets/Scripts/Scripts_Object/Script_Alterable.cs
+++ Assets/Scripts/Scripts_Object/Script_Alterable.cs
@@ -3,16 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Script_Alter_Wall_Security))]
+[RequireComponent(typeof(Script_ItemInfo))]
 public class Script_Alterable : MonoBehaviour
 {
-    public Script_Scriptable_Item item_scriptable;
+    private Script_Scriptable_Item item_scriptable;
 
     private void Start()
     {
-        if(GetComponent<Script_Distortable>())
-        {
-            item_scriptable = GetComponent<Script_Distortable>().GetScriptableItem();
-        }
+        item_scriptable = GetComponent<Script_ItemInfo>().GetItemInfo();
     }
 
     public Script_Scriptable_Item GetScriptableItem()
--- Assets/Scripts/Scripts_Object/Script_Distortable.cs
+++ Assets/Scripts/Scripts_Object/Script_Distortable.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 using cakeslice ;
 
 [RequireComponent(typeof(Outline))]
+[RequireComponent(typeof(Script_ItemInfo))]
 public class Script_Distortable : MonoBehaviour
 {
-    public Script_Scriptable_Item item_scriptable;
+    private Script_Scriptable_Item item_scriptable;
+
+    private void Start()
+    {
+        item_scriptable = GetComponent<Script_ItemInfo>().GetItemInfo();
+    }
 
     public Script_Scriptable_Item GetScriptableItem()
     {
